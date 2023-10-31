8c1437debf0ff3674d41d93c98f95c40b46b19b3
neribruno <brunoneri.ds@gmail.com>
2023-10-31 17:25:39 -0300

    desafio completo.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Olá mundão!");
+//# 1️⃣ Desafio Classificador de nível de Herói
+
+let heroi = "Brunão"
+let xp = 2000
+var nivel;
+
+if (xp <=1000) {
+    nivel = "Ferro";
+}else if(xp >= 1001 && xp <= 2000){
+    nivel = "Bronze";
+}else if(xp >= 2001 && xp <= 5000){
+    nivel = "Prata";
+}else if(xp >= 5001 && xp <= 7000){
+    nivel = "Ouro";
+}else if(xp >= 7001 && xp <= 8000){
+    nivel = "platina";
+}else if(xp >= 8001 && xp <=9000){
+    nivel = "Ascendente";
+}else if(xp >= 9001 && xp <=10000){
+    nivel = "Imortal";
+}else {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + heroi + " está no nível " + nivel);
